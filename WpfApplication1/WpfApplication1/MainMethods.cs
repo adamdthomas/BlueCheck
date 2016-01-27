688eb182452a806dd76a4ef2d65e41fe1f534d4e
Adam Thomas <adamdthomas@gmail.com>
2016-01-27 16:48:41 -0500

    Initial push

---
+++ WpfApplication1/WpfApplication1/MainMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlueChecker
+{
+    public static class MainMethods
+    {
+        public static Thread thread = new Thread(new ThreadStart(Checker));
+        public static bool isStarted = false;
+
+        public static void Checker()
+        {
+            while (isStarted)
+            {
+
+                Thread.Sleep(1000);
+
+                try
+                {
+                    
+                    // do any background work
+                }
+                catch (Exception ex)
+                {
+                    // log errors
+                }
+            }
+
+        }
+
+
+    }
+}
---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace BlueChecker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btStartStop_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            if (MainMethods.isStarted)
+            {
+                //if its started, then stop it
+                
+                MainMethods.thread.Abort();
+                MainMethods.isStarted = false;
+                btStartStop.Content = "Start";
+            }
+            else
+            {
+                MainMethods.thread.Start();
+                MainMethods.isStarted = true;
+                btStartStop.Content = "Stop";
+            }
+
+            
+        }
+    }
+}

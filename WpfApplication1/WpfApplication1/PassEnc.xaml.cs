b3c4a34bc7a8460bc77e6bdd514f5f1d6a232ca5
Adam Thomas <adamdthomas@gmail.com>
2016-05-11 16:14:09 -0400

    Updating password encryptor

--- WpfApplication1/WpfApplication1/PassEnc.xaml.cs
+++ WpfApplication1/WpfApplication1/PassEnc.xaml.cs
@@ -17,8 +17,7 @@ namespace BlueChecker
         {
 
             byte[] myBytes = Encoding.ASCII.GetBytes("leach");
-            string EncryptBSPassword = Encryption.SimpleEncryptWithPassword(txPasswordBS.Text, MainWindow.CurConfig["username"], myBytes);
-            MainWindow.CurConfig["password"] = EncryptBSPassword;
+
 
             string EncryptGMPassword = Encryption.SimpleEncryptWithPassword(txPasswordGM.Text, MainWindow.CurConfig["gmailusername"], myBytes);
             MainWindow.CurConfig["gmailpassword"] = EncryptGMPassword;
@@ -28,10 +27,27 @@ namespace BlueChecker
 
             ConfigData newConf = new ConfigData();
             MainWindow.CurConfig = newConf.GetConfigData();
-            
+
+        }
+
+        private void buttonBS_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] myBytes = Encoding.ASCII.GetBytes("leach");
+            string EncryptBSPassword = Encryption.SimpleEncryptWithPassword(txPasswordBS.Text, MainWindow.CurConfig["username"], myBytes);
+            MainWindow.CurConfig["password"] = EncryptBSPassword;
+
+
+            ConfigData.SetConfigData();
+
+            ConfigData newConf = new ConfigData();
+            MainWindow.CurConfig = newConf.GetConfigData();
+
 
             
+        }
 
+        private void btClose_Click(object sender, RoutedEventArgs e)
+        {
             this.Close();
         }
     }
